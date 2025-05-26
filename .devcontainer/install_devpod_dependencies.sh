672fc0cc478942f8c63b54e74f71c14c4c2d8792
shinjeongtae <shinjeongtae@gmail.com>
2025-05-26 14:24:43 +0000

    [FIX] remove null apt-get install

--- .devcontainer/install_devpod_dependencies.sh
+++ .devcontainer/install_devpod_dependencies.sh
@@ -18,7 +18,6 @@ fi
 # Install kubernetes client
 apt-get update
 # apt-transport-https may be a dummy package; if so, you can skip that package
-apt-get install -y 
 curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update && apt-get install -y kubectl
